4f562ac34e800ce25d430a1bbf2b5fb024acd197
Benedicto Boseke <benedictoboseke@gmail.com>
2022-12-09 18:14:13 +0700

    database core and authentication

---
+++ controller/AuthController.php
@@ -0,0 +1,28 @@
+<?php
+    require_once('./connection.php');
+    if($_SERVER['request_method'] === 'POST'){
+        // handle POST
+        if(isset($_POST['uesrname']) && isset($_POST['password'])) {
+            // user mau login
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+
+            $query = "SELECT * FROM users WHERE
+            username = '$username' AND password = '$password';";
+
+            $result = $connection -> query($query);
+            if ($result -> num_rows > 0){
+                $row = $result -> fetch_assoc();
+                var_dump($row);
+
+            } else {
+                var_dump("Wrong Credentials, please try again");
+                
+            }
+
+        }
+    }
+    if($_SERVER['request_method'] === 'GET'){
+        // handle GET
+    }
+?>
